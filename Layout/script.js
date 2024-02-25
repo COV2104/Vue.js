8b03d0fbcfa38ef160f83044dc47f1c57e8c53ec
COV-2104 <tsehlo@mail.ru>
2024-02-25 16:35:29 +0300

    Макет-домашнее задание № 2

---
+++ Layout/script.js
@@ -0,0 +1,49 @@
+new Vue({
+  el: "#app",
+  data: {
+    blogItems: [
+      {
+        image: "./img/article_1.jpg",
+        alt: "Kitchan Design",
+        description: "Kitchan Design",
+        title: "Let’s Get Solution For Building Construction Work",
+        date: "26 December,2022",
+      },
+      {
+        image: "./img/article_2.jpg",
+        alt: "Living Design",
+        description: "Living Design",
+        title: "Low Cost Latest Invented Interior Designing Ideas.",
+        date: "22 December,2022",
+      },
+      {
+        image: "./img/article_3.jpg",
+        alt: "Interior Design",
+        description: "Interior Design",
+        title: "Best For Any Office [&] Business Interior Solution",
+        date: "25 December,2022",
+      },
+      {
+        image: "./img/article_4.jpg",
+        alt: "Kitchan Design",
+        description: "Kitchan Design",
+        title: "Let’s Get Solution For Building Construction Work",
+        date: "26 December,2022",
+      },
+      {
+        image: "./img/article_5.jpg",
+        alt: "Living Design",
+        description: "Living Design",
+        title: "Low Cost Latest Invented Interior Designing Ideas.",
+        date: "22 December,2022",
+      },
+      {
+        image: "./img/article_6.jpg",
+        alt: "Interior Design",
+        description: "Interior Design",
+        title: "Best For Any Office [&] Business Interior Solution",
+        date: "25 December,2022",
+      },
+    ],
+  },
+});
